26a5a236320dc87d56470b04c0bfe3748bcfe5d6
bradhilton <brad.hilton.nw@gmail.com>
2016-04-12 19:30:37 -0600

    Initial Commit

---
+++ Symphony/Symphony.h
@@ -0,0 +1,19 @@
+//
+//  Symphony.h
+//  Symphony
+//
+//  Created by Bradley Hilton on 4/12/16.
+//  Copyright © 2016 Brad Hilton. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for Symphony.
+FOUNDATION_EXPORT double SymphonyVersionNumber;
+
+//! Project version string for Symphony.
+FOUNDATION_EXPORT const unsigned char SymphonyVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Symphony/PublicHeader.h>
+
+
